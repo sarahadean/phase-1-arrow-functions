51e1a82fc60c8aecac38e4f714cb945237b6af19
Sarah Dean <sarah.schmidtdean@gmail.com>
2023-04-13 18:45:34 -0500

    arrow functions

---
+++ index.js
@@ -0,0 +1,9 @@
+// Function expression called divide
+const divide = (number) => (number / 100)
+console.log(divide(2000))
+
+//Function called square
+const square = (squareNumber) => (squareNumber ** squareNumber)
+
+//adds two parameters together
+const add = (num1, num2) => (num1 + num2)
